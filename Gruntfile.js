48eb2dc19449f0975b86dac7ecd1a69b51423858
Charles Julian Knight <charles@rabidaudio.com>
2014-08-30 23:57:05 -0400

    painstakingly rebuilt everything to get yo to play right

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ var mountFolder = function (connect, dir) {
 // 'test/spec/{,*/}*.js'
 // use this if you want to match all subfolders:
 // 'test/spec/**/*.js'
-// templateFramework: 'lodash'
+// templateFramework: 'handlebars'
 
 module.exports = function (grunt) {
     // show elapsed time at the end
@@ -46,19 +46,19 @@ module.exports = function (grunt) {
                     '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
                     '<%= yeoman.app %>/scripts/templates/*.{ejs,mustache,hbs}',
-                    'test/spec/**/*.js'
+                    //'test/spec/**/*.js'
                 ]
             },
-            jst: {
+            handlebars: {
                 files: [
-                    '<%= yeoman.app %>/scripts/templates/*.ejs'
+                    '<%= yeoman.app %>/scripts/templates/*.hbs'
                 ],
-                tasks: ['jst']
+                tasks: ['handlebars']
             },
-            test: {
-                files: ['<%= yeoman.app %>/scripts/{,*/}*.js', 'test/spec/**/*.js'],
-                tasks: ['test:true']
-            }
+            //test: {
+            //    files: ['<%= yeoman.app %>/scripts/{,*/}*.js', 'test/spec/**/*.js'],
+            //    tasks: ['test:true']
+            //}
         },
         connect: {
             options: {
@@ -77,7 +77,7 @@ module.exports = function (grunt) {
                     }
                 }
             },
-            test: {
+            /*test: {
                 options: {
                     port: 9001,
                     middleware: function (connect) {
@@ -89,7 +89,7 @@ module.exports = function (grunt) {
                         ];
                     }
                 }
-            },
+            },*/
             dist: {
                 options: {
                     middleware: function (connect) {
@@ -104,9 +104,9 @@ module.exports = function (grunt) {
             server: {
                 path: 'http://localhost:<%= connect.options.port %>'
             },
-            test: {
-                path: 'http://localhost:<%= connect.test.options.port %>'
-            }
+            //test: {
+            //    path: 'http://localhost:<%= connect.test.options.port %>'
+            //}
         },
         clean: {
             dist: ['.tmp', '<%= yeoman.dist %>/*'],
@@ -121,17 +121,17 @@ module.exports = function (grunt) {
                 'Gruntfile.js',
                 '<%= yeoman.app %>/scripts/{,*/}*.js',
                 '!<%= yeoman.app %>/scripts/vendor/*',
-                'test/spec/{,*/}*.js'
+                //'test/spec/{,*/}*.js'
             ]
         },
-        mocha: {
+        /*mocha: {
             all: {
                 options: {
                     run: true,
-                    src: ['http://localhost:<%= connect.test.options.port %>/index.html']
+                    urls: ['http://localhost:<%= connect.test.options.port %>/index.html']
                 }
             }
-        },
+        },*/
         compass: {
             options: {
                 sassDir: '<%= yeoman.app %>/styles',
@@ -158,7 +158,7 @@ module.exports = function (grunt) {
                     paths: {
                         'templates': '../../.tmp/scripts/templates',
                         'jquery': '../../<%= yeoman.app %>/bower_components/jquery/dist/jquery',
-                        'underscore': '../../<%= yeoman.app %>/bower_components/underscore/underscore',
+                        'underscore': '../../<%= yeoman.app %>/bower_components/lodash/dist/lodash',
                         'backbone': '../../<%= yeoman.app %>/bower_components/backbone/backbone'
                     },
                     // TODO: Figure out how to make sourcemaps work with grunt-usemin
@@ -167,8 +167,7 @@ module.exports = function (grunt) {
                     // required to support SourceMaps
                     // http://requirejs.org/docs/errors.html#sourcemapcomments
                     preserveLicenseComments: false,
-                    useStrict: true,
-                    wrap: true
+                    useStrict: true
                     //uglify2: {} // https://github.com/mishoo/UglifyJS2
                 }
             }
@@ -249,13 +248,14 @@ module.exports = function (grunt) {
                 rjsConfig: '<%= yeoman.app %>/scripts/main.js'
             }
         },
-        jst: {
-            options: {
-                amd: true
-            },
+        handlebars: {
             compile: {
+                options: {
+                    namespace: 'JST',
+                    amd: true
+                },
                 files: {
-                    '.tmp/scripts/templates.js': ['<%= yeoman.app %>/scripts/templates/*.ejs']
+                    '.tmp/scripts/templates.js': ['<%= yeoman.app %>/scripts/templates/*.hbs']
                 }
             }
         },
@@ -288,22 +288,22 @@ module.exports = function (grunt) {
             return grunt.task.run(['build', 'open:server', 'connect:dist:keepalive']);
         }
 
-        if (target === 'test') {
+        /*if (target === 'test') {
             return grunt.task.run([
                 'clean:server',
                 'createDefaultTemplate',
-                'jst',
+                'handlebars',
                 'compass:server',
                 'connect:test',
                 'open:test',
                 'watch'
             ]);
-        }
+        }*/
 
         grunt.task.run([
             'clean:server',
             'createDefaultTemplate',
-            'jst',
+            'handlebars',
             'compass:server',
             'connect:livereload',
             'open:server',
@@ -311,12 +311,12 @@ module.exports = function (grunt) {
         ]);
     });
 
-    grunt.registerTask('test', function (isConnected) {
+    /*grunt.registerTask('test', function (isConnected) {
         isConnected = Boolean(isConnected);
         var testTasks = [
                 'clean:server',
                 'createDefaultTemplate',
-                'jst',
+                'handlebars',
                 'compass',
                 'connect:test',
                 'mocha',
@@ -329,12 +329,12 @@ module.exports = function (grunt) {
             testTasks.splice(testTasks.indexOf('connect:test'), 1);
             return grunt.task.run(testTasks);
         }
-    });
+    });*/
 
     grunt.registerTask('build', [
         'clean:dist',
         'createDefaultTemplate',
-        'jst',
+        'handlebars',
         'compass:dist',
         'useminPrepare',
         'requirejs',
@@ -350,7 +350,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', [
         'jshint',
-        'test',
+        //'test',
         'build'
     ]);
 };
